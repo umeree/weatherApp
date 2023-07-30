326a68da78e441d07f97818b7ca9cb5e7db03850
umeree <umernadeam@gmail.com>
2023-07-30 21:01:33 +0500

    fetcing completed

--- data.js
+++ data.js
@@ -6,6 +6,11 @@ async function checkWeather() {
   const response = await fetch(apiUrl + `&appid=${apikey}`);
   var data = await response.json();
 
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = data.main.temp;
+  document.querySelector(".humidity").innerHTML = data.main.humidity;
+  document.querySelector(".wind").innerHTML = data.wind.speed;
+
   console.log(data);
 }
 checkWeather();
